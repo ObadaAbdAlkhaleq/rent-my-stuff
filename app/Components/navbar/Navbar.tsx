8cd59849b7f652fb636078ec1af565d0f8d6ddd2
ObadaAbdAlkhaleq <83885520+ObadaAbdAlkhaleq@users.noreply.github.com>
2023-05-23 09:39:28 +0300

    fix

--- app/Components/navbar/Navbar.tsx
+++ app/Components/navbar/Navbar.tsx
@@ -19,7 +19,7 @@ const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
     //  max-w-screen-lg mx-auto 
 
     return (
-        <div className="fixed w-full bg-white z-10 shadow-sm">
+        <div className="fixed w-full bg-white z-30 shadow-sm">
             <div className=" py-4 border-b-[1px]">
                 <Container>
                     <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
