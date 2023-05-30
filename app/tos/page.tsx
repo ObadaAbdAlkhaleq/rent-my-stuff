d665fa060d2809b4f052b09d6e3ae522a96085ec
ObadaAbdAlkhaleq <83885520+ObadaAbdAlkhaleq@users.noreply.github.com>
2023-05-30 20:58:10 +0300

    feat(Add): ToS/ Policies Page

---
+++ app/tos/page.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+const ToSPage = () => {
+  return (
+    <div className="font-sans pt-24">
+      <div className="bg-gray-100 py-8 px-4 sm:px-6 lg:px-8">
+        <div className="font-sans container mx-auto px-4">
+          <div className="">
+            <h1 className="text-4xl font-extrabold mb-2">Terms of Services</h1>
+            <p className="font-sans mb-8">Learn about our Terms and Policies</p>
+          </div>
+          <div className="ml-8">
+            <h2 className="font-sans text-2xl font-semibold mb-4">Pickup and Return Policy:</h2>
+            <p className="font-sans mb-4">
+              Users are required to pick up the reserved item on the scheduled date and time at the designated pickup location. Late pickups may result in additional charges or cancellation of the reservation.
+            </p>
+            <p className="font-sans mb-4">
+              Upon returning the item, users must ensure it is in the same condition as received, free from any damage or excessive wear. Any damages or missing items may result in charges or loss of security deposit.
+            </p>
+
+            <h2 className="font-sans text-2xl font-semibold mb-4">Item Reservation Policy:</h2>
+            <p className="font-sans mb-4">
+              Users can reserve items for a specified duration, subject to availability. Reservations can be made through the website, and users will receive a confirmation email with the reservation details.
+            </p>
+            <p className="font-sans mb-4">
+              To secure a reservation, users may be required to provide a valid payment method or pay a deposit, which will be refunded upon successful return of the item.
+            </p>
+
+            <h2 className="font-sans text-2xl font-semibold mb-4">Listing Items Policy:</h2>
+            <p className="font-sans mb-4">
+              Users can list their items for rent on the platform, providing accurate descriptions, high-quality images, and any necessary specifications or usage instructions.
+            </p>
+            <p className="font-sans mb-4">
+              It is the responsibility of the user listing the item to ensure that it is in good working condition, meets safety standards, and complies with any applicable laws or regulations.
+            </p>
+            <p className="font-sans mb-4">
+              Users are encouraged to provide clear guidelines on pickup and return procedures, any special considerations or restrictions, and any additional terms specific to their listed items.
+            </p>
+
+            <h2 className="font-sans text-2xl font-semibold mb-4">Cancellation Policy:</h2>
+            <p className="font-sans mb-4">
+              Users can cancel their reservation free of charge if the cancellation is made within a specified time frame, such as 3 days before the scheduled pickup date. Cancellations made after this period may be subject to a cancellation fee.
+            </p>
+            <p className="font-sans mb-4">
+              In the event that the user needs to cancel due to unforeseen circumstances or emergencies, they are encouraged to contact the platform's customer support for assistance and to discuss any possible options or alternatives.
+            </p>
+
+            <h2 className="font-sans text-2xl font-semibold mb-4">Terms of Use:</h2>
+            <p className="font-sans mb-4">
+              Users must carefully read and agree to the platform's terms of use before accessing or using the services provided. The terms of use outline the rights and responsibilities of both users and the platform.
+            </p>
+            <p className="font-sans mb-4">
+              Users are expected to comply with all applicable local laws and regulations while using the platform and renting items. Any violation of the terms of use or misuse of the platform may result in account suspension or termination.
+            </p>
+          </div></div>
+      </div>
+
+    </div>
+  );
+};
+
+export default ToSPage;
